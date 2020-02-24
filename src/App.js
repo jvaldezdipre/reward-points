7c6a15d7095a2f6db67685c1de1de5f84dfbe05c
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-02-24 13:20:42 -0500

    Reward Points

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Table from "./component/CustomerTable";
+import "./App.css";
+import "react-table/react-table.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Table />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/dataSet.js
@@ -0,0 +1,100 @@
+export default function() {
+  return Promise.resolve([
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 150,
+      transactionDate: "02-01-2020"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 80,
+      transactionDate: "02-24-2020"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 60,
+      transactionDate: "01-13-2020"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 10,
+      transactionDate: "06-01-2019"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 75,
+      transactionDate: "06-21-2019"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 2,
+      transactionDate: "07-01-2019"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 1,
+      transactionDate: "07-04-2019"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 80,
+      transactionDate: "07-03-2019"
+    },
+    {
+      custid: 1,
+      name: "Jonathan",
+      amount: 280,
+      transactionDate: "07-21-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 230,
+      transactionDate: "05-01-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 55,
+      transactionDate: "05-21-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 10,
+      transactionDate: "06-01-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 75,
+      transactionDate: "06-21-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 300,
+      transactionDate: "07-01-2019"
+    },
+    {
+      custid: 2,
+      name: "Jeffrey",
+      amount: 224,
+      transactionDate: "07-21-2019"
+    },
+    {
+      custid: 3,
+      name: "Anthony",
+      amount: 150,
+      transactionDate: "06-21-2019"
+    }
+  ]);
+}
